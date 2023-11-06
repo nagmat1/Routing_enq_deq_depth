408f98c34461a9f6cbe1942eb2a075fa7c3845ea
nagmat <nagmat@gmail.com>
2023-11-06 14:23:09 -0600

    Update hex_rec_data.py

--- receiver2/hex_rec_data.py
+++ receiver2/hex_rec_data.py
@@ -15,11 +15,11 @@ def convert_to_int(list_val):
     return int("".join(list_val), 16)
 
 if __name__ == '__main__':
-    out_file = 'barlag.txt'
+    out_file = 'barlag.csv'
     rcvd_pkts = []
     rcvd_packets_loads = []
 
-    sniff(filter='proto IGMP ',count=100, iface="enp6s0", prn=lambda pkt: rcvd_packets_loads.append(linehexdump(pkt, dump=True, onlyhex=1)))
+    sniff(filter='proto IGMP ',count=100000, iface="enp6s0", prn=lambda pkt: rcvd_packets_loads.append(linehexdump(pkt, dump=True, onlyhex=1)))
     with open(out_file, "w") as out:
         for pkt in rcvd_packets_loads:
             print("Packet = ",pkt)
@@ -31,4 +31,3 @@ if __name__ == '__main__':
             #out.write(f"deq_depth {tokens[46:50]},{convert_to_int(tokens[46:50])}\n")
             out.write("{},{},{},{}\n".format(convert_to_int(tokens[34:38]),convert_to_int(tokens[38:42]),convert_to_int(tokens[42:46]),convert_to_int(tokens[46:50])))
             #out.write("***************************************\n")
-
