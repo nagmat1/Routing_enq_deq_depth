9fce010731563ef66ed887010c32a152e43252f4
nagmat <nagmat@gmail.com>
2023-12-18 11:52:20 -0600

    Update rules2.sh

--- switch2/rules2.sh
+++ switch2/rules2.sh
@@ -1,3 +1,3 @@
 echo "table_add MyIngress.ipv4_lpm MyIngress.forward 192.168.1.0/24 => 00:00:00:00:00:03 0" | simple_switch_CLI
 echo "table_add MyIngress.ipv4_lpm MyIngress.forward 192.168.2.0/24 => 00:00:00:00:00:06 1" | simple_switch_CLI
-echo "table_add MyEgress.swtrace MyEgress.add_swtrace => 100" | simple_switch_CLI
+echo "table_add MyEgress.swtrace MyEgress.add_swtrace => 200" | simple_switch_CLI
