0ca105b1153bee84d108dd1a86a5e0d73e6ddf91
nagmat <nagmat@gmail.com>
2023-12-21 17:01:28 -0600

    Create multi_hop_rules.sh

---
+++ Multi_hop_routing1/switch/multi_hop_rules.sh
@@ -0,0 +1,3 @@
+echo "table_add MyIngress.ipv4_lpm MyIngress.ipv4_forward 192.168.1.0/24 => 00:00:00:00:00:01 0" | simple_switch_CLI
+echo "table_add MyIngress.ipv4_lpm MyIngress.ipv4_forward 192.168.2.0/24 => 00:00:00:00:00:04 1" | simple_switch_CLI
+echo "table_add MyEgress.swtrace MyEgress.add_swtrace => 100" | simple_switch_CLI
